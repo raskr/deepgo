c87f197949eeb4cbd30c9878a1d5466dab5e3964
perrier1034 <janne.dc.ib.sa@gmail.com>
2015-12-22 15:17:05 +0900

    mod script

--- scripts/data_file.py
+++ scripts/data_file.py
@@ -4,16 +4,11 @@ import numpy as np
 from chainer import cuda
 import os
 
-query = "SELECT state, target FROM white WHERE _id BETWEEN {} AND {} ORDER BY RANDOM()"
-query_test = "SELECT state, target, invalid FROM white WHERE _id BETWEEN {} AND {} ORDER BY RANDOM()"
 
+base_path = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.normpath(os.path.join(base_path, '../file_out/'))
 
 class Data:
-
-    def data_dir(self):
-        base_path = os.path.dirname(os.path.abspath(__file__))
-        files_path = os.path.normpath(os.path.join(base_path, '../file_out/'))
-        return files_path
 
     def __init__(self, use_gpu, n_epoch, n_ch, b_size, n_train_data, n_test_data, db_path):
         self.db_path = db_path
@@ -34,13 +29,13 @@ class Data:
 
     def __call__(self, train, i):
         if train:
-            with open(os.path.join(data_dir(), string(i) + '.sgf'), 'r') as f:
+            with open(os.path.join(data_dir, string(i) + '.sgf'), 'r') as f:
                 xs = str2floats(f.readline())
                 ys = f.readline()
             return self.xp.asarray(xs, dtype=self.xp.float32).reshape(self.b_size, self.n_ch, 19, 19), \
                    self.xp.asarray(ys, dtype=self.xp.int32)
         else:
-            with open(os.path.join(data_dir(), string(i) + '.sgf'), 'r') as f:
+            with open(os.path.join(data_dir, string(i) + '.sgf'), 'r') as f:
                 xs = str2floats(f.readline())
                 ys = f.readline()
                 invalids = str2floats_simple(f.readline())
