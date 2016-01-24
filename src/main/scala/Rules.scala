1bf3358399374cf2fa01175ec340fe2c9c820c9a
perrier1034 <janne.dc.ib.sa@gmail.com>
2016-01-24 14:20:24 -0800

    update

--- src/main/scala/Rules.scala
+++ src/main/scala/Rules.scala
@@ -154,7 +154,7 @@ object Rules {
       board.printState(19, 19, Some(move), None)
       println("bad after ")
       board.printState(19, 19, None, None)
-      throw new RuntimeException("error!!!!!!!!!!!!!!!!!!!")
+      //throw new RuntimeException("error!!!!!!!!!!!!!!!!!!!")
     }
     dst
   }
